2a79163606f2d6294145070cd2845d02d3d14a31
Gilbert Oliveira <contato@gilbert.dev.br>
2024-08-14 19:54:40 -0300

    Query.php

--- index.php
+++ index.php
@@ -4,3 +4,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once 'vendor/autoload.php';
+
+use App\Database\Query;
+
+
+$query = new Query();
+
+$alunos = $query->select(
+    tabela: 'alunos'
+);
+
+dd($alunos);
+
